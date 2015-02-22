505737324faf8ddfaf5a0caca5a8ef1efd0d2b13
seg-s <sseguier@live.ca>
2015-02-22 02:31:20 -0500

    e

--- cloud.js
+++ cloud.js
@@ -1,10 +1,28 @@
 /* cloud.js */
 
+//window.onload = searchDivs;
+document.getElementById("btwiea7ryt3").onclick = searchDivs;
+
 function searchDivs(){
+	var info;
+	document.getElementById("searchContainer").innerHTML = "function";
+	chrome.storage.sync.get("SearchBuddyInfo", function (obj)
+		{
+			info = obj.SearchBuddyInfo;
+		});
+	document.getElementById("searchContainer").innerHTML = "sync";	
+	var parent = document.getElementById("searchContainer");
 	
-	foreach(){
-		
-		document.write(<div class="search"><a class="text"></a></div>);
+	for(i = 0 ; i < info.lengh ; i++){
+		document.getElementById("searchContainer").innerHTML = "looping";
+		parent.appendChild(a);
+		var a = document.createElement(a);
+		a.id="search " + i;
+		var div = document.createElement(div);
+		a.appendChild(div);
+		var p = document.createElement(p);
+		div.appendChild(p);
+		p.innerHTML(info[i].search);
 	}
 	
 }
